425b374d8e446c24f64c904762208bc68749c32a
Suryanarayan Das <das278987@gmail.com>
2024-10-06 19:38:22 +0530

    new file:   print_no_recursion.cpp 	new file:   print_no_recursion.exe git commit

---
+++ print_no_recursion.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*int printno(int i,int n){
+    if(i>n)
+      return 0;
+      cout<<i<<endl;
+      printno(i+1,n);
+}*/
+int printno1(int i,int n){
+    if(i<1)
+    return 0;
+    printno1(i-1,n);
+    cout<<i<<endl;
+}
+int main(){
+    int  n;
+    cout<<"enter the value of n:";
+    cin>>n;
+    // printno(1,n);
+    printno1(n,n);
+    return 0;
+}
